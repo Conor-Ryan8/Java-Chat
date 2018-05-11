5c613a18df2e7d20cb7346a45c641087a5f8ac5d
Conor Ryan <37616617+conorjcryan@users.noreply.github.com>
2018-05-11 16:03:51 +0100

    Update and rename ThreadedServer.java to MultithreadedClientServerClass.java

--- MultithreadedClientServerClass.java
+++ MultithreadedClientServerClass.java
@@ -1,21 +1,13 @@
-/********************************************
- * Conor Ryan - Socket Programming - Java   *
- * Threaded Server Class                    *
- * April 2018                               *
- ********************************************/
-
 package testtest;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class Server {
 	
 	private final static int PACKETSIZE = 10;
 	static DatagramSocket socket = null;
-	static InetAddress address;
 	int Read = 1;
 	int portin;
 	int portout;
@@ -25,15 +17,7 @@ public class Server {
 	{  
 		this.portin = portin;
 		this.portout = portout;
-		this.IP = IP;
-		try 
-		{
-			address = InetAddress.getByName(IP);
-		} 
-		catch (UnknownHostException e) 
-		{
-			e.printStackTrace();
-		}		
+		this.IP = IP;			
 	}
 	public void ConnectSocket()
 	{   
@@ -51,6 +35,7 @@ public class Server {
 	{	   
 		try 
 		{	
+			InetAddress address = InetAddress.getByName(IP);
 			DatagramPacket packet = new DatagramPacket(send, send.length, address, portout);
 			socket.send(packet);
 			packet.setData(new byte[PACKETSIZE]);
@@ -61,7 +46,7 @@ public class Server {
 		}
 	}
 	
-	public void ServerStart() throws Exception
+	public void StartListening() throws Exception
 	{	
 		Thread get = new Thread( new Runnable() 
 		{
@@ -75,11 +60,8 @@ public class Server {
 					while (Read == 1) 
 					{
 						System.out.println("Server Waiting");
-						
 						byte[] data = getData();
-						
 						System.out.println("Server Received data!");
-						//incoming data contained in "data" byte array
 					}
 				} 
 				catch (Exception e) 
@@ -91,13 +73,12 @@ public class Server {
 		get.start();			
 	}
 	
-	public byte[] getData() 
+	private byte[] getData() 
 	{
-		DatagramPacket packet = null;
 		byte[] receive = new byte[10];
 		try 
 		{
-			packet = new DatagramPacket(receive, PACKETSIZE);
+			DatagramPacket packet = new DatagramPacket(receive, PACKETSIZE);
 			socket.receive(packet);
 		}
 		catch (Exception e) 
